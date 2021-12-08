91bc0623d55527534f195bef06ae3f418e99aa04
THEfireZz <maximedhaeyere@gmail.com>
2021-12-08 15:01:43 +0100

    modif pions

--- Reversi/src/Methode_Main.java
+++ Reversi/src/Methode_Main.java
@@ -1,10 +1,27 @@
+import java.util.ArrayList;
+
 public class Methode_Main {
-    public static void afficherTableau(String[][]tab){
+    public static void afficherTableau(String[][]tab,int[]coSelectionner,ArrayList<Integer[]>listeCo ){
         for (int i = 0; i <tab.length ; i++) {
             for (int j = 0; j <tab[1].length ; j++) {
                 System.out.print(tab[i][j]+"\t");
             }
             System.out.println();
+        }
+    }
+    public static void listDesCoJouables(String[][]tab,int joueur){
+        ArrayList<Integer[]> listeCo = new ArrayList<Integer[]>();
+        for (int i = 0; i <tab.length ; i++) {
+            for (int j = 0; j <tab[1].length ; j++) {
+                if (joueur==1){
+                    if (tab[i][j].equals("⬡")){
+
+                    }
+                }else {
+
+                }
+            }
+
         }
     }
 }
