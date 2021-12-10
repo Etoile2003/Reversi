90633f725e1a4afbcb7b35e68a2b00227b5edea3
Etoile2003 <bastien.pelletier01@gmail.com>
2021-12-10 11:32:10 +0100

    cursor movement fix v2

--- Reversi/src/Game.java
+++ Reversi/src/Game.java
@@ -1,11 +1,14 @@
 public class Game {
     public static boolean playGame= true ;
+    public static int turn = 0 ;
     public static void myGame(){
         Methode_Bastien.keyListener();
 
 
 
         while (playGame){
+            Methode_Bastien.canPlay = true ;
+
 
 
 
