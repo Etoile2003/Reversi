970add688a406552b1df5a3df225ef6f30557c20
Etoile2003 <bastien.pelletier01@gmail.com>
2021-12-17 11:01:27 +0100

    retourner les jeton trop bien

--- Reversi/src/Methode_Main.java
+++ Reversi/src/Methode_Main.java
@@ -7,7 +7,7 @@ public class Methode_Main {
             System.out.print(i + 1);
             for (int j = 0; j < tab[0].length; j++) {
                 boolean find = false  ;
-                for (int k = 0; k <listCo.size() ; k+=3) {
+                for (int k = 0; k <listCo.size() ; k+=2) {
                     if (listCo.get(k)==i&&listCo.get(k+1)==j && !find){
                         System.out.print("\u001B[31m"+"X"+"\u001B[0m"+"\t");
                         find = true ;
@@ -33,7 +33,7 @@ public class Methode_Main {
                 if (joueur == 1) {
                     if (tab[i][j].equals("⬡")) {
                         int compteur;
-                        int[] cojouable = new int[3];
+                        int[] cojouable = new int[2];
 
                         compteur = 0;
                         verifjouable(Menu.plateau, i, j, -1, -1, compteur, "⬢", cojouable, myArrayList,listEmplacementDepart,nombreJetonsRetourner);
@@ -68,7 +68,7 @@ public class Methode_Main {
                 } else {
                     if (tab[i][j].equals("⬢")) {
                         int compteur;
-                        int[] cojouable = new int[3];
+                        int[] cojouable = new int[2];
 
 
                         compteur = 0;
@@ -118,7 +118,7 @@ public class Methode_Main {
     public static void verifjouable(String[][] tab, int pionsX, int pionsY, int modx, int mody, int compteur, String pion, int[] coPositionJouable, ArrayList listCo,ArrayList listCoDepart, ArrayList nombreJeton) {
         coPositionJouable[0] = -1;
         coPositionJouable[1] = -1;
-        coPositionJouable[2] = -1;
+
         try {
             int newPosX = pionsX + modx, newposY = pionsY + mody;
             if (tab[newPosX][newposY].equals(pion)) {
