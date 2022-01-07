38a10254efdb0b3f30ba98737f7588bf0e3eec61
thefi <maximedhaeyere@gmail.com>
2022-01-07 14:14:56 +0100

    Clean légé du code

--- Reversi/src/Menu.java
+++ Reversi/src/Menu.java
@@ -15,9 +15,9 @@ public class Menu {
                     {".",".",".",".",".",".",".","."}};
     public static int[][]tabint =
                     {{0,0,0,0,0,0,0,0},
-                    {0,0,0,0,0,0,0,0},          // changer la tab de string pour un tab de int
-                    {0,0,0,0,0,0,0,0},          //1 => noire 2 => blanc 0 => vide -1 => case jouable
-                    {0,0,0,1,2,0,0,0},
+                    {0,0,0,0,0,0,2,1},          // changer la tab de string pour un tab de int
+                    {0,0,0,0,2,2,0,0},          //1 => noire 2 => blanc 0 => vide -1 => case jouable
+                    {0,0,0,1,2,0,1,0},
                     {0,0,0,2,1,0,0,0},
                     {0,0,0,0,0,0,0,0},
                     {0,0,0,0,0,0,0,0},
