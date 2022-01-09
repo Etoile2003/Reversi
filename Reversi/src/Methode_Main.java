1cd4444af6798abaf7786fbacb115ef3b188f552
thefi <maximedhaeyere@gmail.com>
2022-01-09 12:30:16 +0100

    controle al L

--- Reversi/src/Methode_Main.java
+++ Reversi/src/Methode_Main.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
 import java.util.List;
+
 public class Methode_Main {
     public static void afficherTableau(int[][] tab, List<ArrayList> listCo) {
         for (int i = 0; i < tab.length; i++) {
-            System.out.print(i + 1+"\t");
+            System.out.print(i + 1 + "\t");
             for (int j = 0; j < tab[1].length; j++) {
                 if (tab[i][j] == 1) {
                     System.out.print("⬡" + "\t");
@@ -14,22 +15,23 @@ public class Methode_Main {
                 } else {
                     System.out.print("\u001B[31m" + "X" + "\u001B[0m" + "\t");
                 }
-            }System.out.println();
+            }
+            System.out.println();
         }
         System.out.println(" \t1   2   3   4   5   6   7   8");
         for (int k = 0; k < listCo.size(); k++) {
-                System.out.println(listCo.size());
-                ArrayList<Integer> cojouable = listCo.get(k);
-                System.out.println("X " + cojouable.get(0));
-                System.out.println("Y " + cojouable.get(1));
-                System.out.println("Departx " + cojouable.get(2));
-                System.out.println("Departy " + cojouable.get(3));
-                System.out.println("nbrjeton " + cojouable.get(4));
-                System.out.println("------------------------------------");
-            }
-        for (int i = 0; i <tab.length ; i++) {
+            System.out.println(listCo.size());
+            ArrayList<Integer> cojouable = listCo.get(k);
+            System.out.println("X " + cojouable.get(0));
+            System.out.println("Y " + cojouable.get(1));
+            System.out.println("Departx " + cojouable.get(2));
+            System.out.println("Departy " + cojouable.get(3));
+            System.out.println("nbrjeton " + cojouable.get(4));
+            System.out.println("------------------------------------");
+        }
+        for (int i = 0; i < tab.length; i++) {
             for (int j = 0; j < tab[1].length; j++) {
-                System.out.print(tab[i][j]+"\t");
+                System.out.print(tab[i][j] + "\t");
             }
             System.out.println();
         }
@@ -45,59 +47,58 @@ public class Methode_Main {
             for (int j = 0; j < tab[1].length; j++) {
                 if (joueur == 1) {
                     if (tab[j][i] == 1) {
-                        System.out.println("pions trouver en "+j+" "+i);
+                        System.out.println("pions trouver en " + j + " " + i);
                         compteur = 0;
-                        verifjouable(Menu.tabint, i,j, -1, -1, compteur, 2, ListeCoJouable, CoDepX, CoDepY);
+                        verifjouable(Menu.tabint, i, j, -1, -1, compteur, 2, ListeCoJouable, CoDepX, CoDepY);
 
                         compteur = 0;
-                        verifjouable(Menu.tabint, i,j, 1, -1, compteur, 2, ListeCoJouable, CoDepX, CoDepY);
+                        verifjouable(Menu.tabint, i, j, 1, -1, compteur, 2, ListeCoJouable, CoDepX, CoDepY);
 
                         compteur = 0;
-                        verifjouable(Menu.tabint, i,j, -1, 1, compteur, 2, ListeCoJouable, CoDepX, CoDepY);
+                        verifjouable(Menu.tabint, i, j, -1, 1, compteur, 2, ListeCoJouable, CoDepX, CoDepY);
 
                         compteur = 0;
-                        verifjouable(Menu.tabint, i,j, 1, 1, compteur, 2, ListeCoJouable, CoDepX, CoDepY);
+                        verifjouable(Menu.tabint, i, j, 1, 1, compteur, 2, ListeCoJouable, CoDepX, CoDepY);
 
                         compteur = 0;
-                        verifjouable(Menu.tabint, i,j, -1, 0, compteur, 2, ListeCoJouable, CoDepX, CoDepY);
+                        verifjouable(Menu.tabint, i, j, -1, 0, compteur, 2, ListeCoJouable, CoDepX, CoDepY);
 
                         compteur = 0;
-                        verifjouable(Menu.tabint, i,j, 1, 0, compteur, 2, ListeCoJouable, CoDepX, CoDepY);
+                        verifjouable(Menu.tabint, i, j, 1, 0, compteur, 2, ListeCoJouable, CoDepX, CoDepY);
 
                         compteur = 0;
-                        verifjouable(Menu.tabint, i,j, 0, -1, compteur, 2, ListeCoJouable, CoDepX, CoDepY);
+                        verifjouable(Menu.tabint, i, j, 0, -1, compteur, 2, ListeCoJouable, CoDepX, CoDepY);
 
                         compteur = 0;
-                        verifjouable(Menu.tabint, i,j, 0, 1, compteur, 2, ListeCoJouable, CoDepX, CoDepY);
+                        verifjouable(Menu.tabint, i, j, 0, 1, compteur, 2, ListeCoJouable, CoDepX, CoDepY);
                     }
-                }
-                else {
+                } else {
                     if (tab[i][j] == 2) {
-                        System.out.println("pions trouver en "+j+" "+i);
+                        System.out.println("pions trouver en " + j + " " + i);
 
                         compteur = 0;
-                        verifjouable(Menu.tabint, i,j, -1, -1, compteur, 1, ListeCoJouable, CoDepX, CoDepY);
+                        verifjouable(Menu.tabint, i, j, -1, -1, compteur, 1, ListeCoJouable, CoDepX, CoDepY);
 
                         compteur = 0;
-                        verifjouable(Menu.tabint, i,j, 1, -1, compteur, 1, ListeCoJouable, CoDepX, CoDepY);
+                        verifjouable(Menu.tabint, i, j, 1, -1, compteur, 1, ListeCoJouable, CoDepX, CoDepY);
 
                         compteur = 0;
-                        verifjouable(Menu.tabint, i,j, -1, 1, compteur, 1, ListeCoJouable, CoDepX, CoDepY);
+                        verifjouable(Menu.tabint, i, j, -1, 1, compteur, 1, ListeCoJouable, CoDepX, CoDepY);
 
                         compteur = 0;
-                        verifjouable(Menu.tabint, i,j, 1, 1, compteur, 1, ListeCoJouable, CoDepX, CoDepY);
+                        verifjouable(Menu.tabint, i, j, 1, 1, compteur, 1, ListeCoJouable, CoDepX, CoDepY);
 
                         compteur = 0;
-                        verifjouable(Menu.tabint, i,j, -1, 0, compteur, 1, ListeCoJouable, CoDepX, CoDepY);
+                        verifjouable(Menu.tabint, i, j, -1, 0, compteur, 1, ListeCoJouable, CoDepX, CoDepY);
 
                         compteur = 0;
-                        verifjouable(Menu.tabint, i,j, 1, 0, compteur, 1, ListeCoJouable, CoDepX, CoDepY);
+                        verifjouable(Menu.tabint, i, j, 1, 0, compteur, 1, ListeCoJouable, CoDepX, CoDepY);
 
                         compteur = 0;
-                        verifjouable(Menu.tabint, i,j, 0, -1, compteur, 1, ListeCoJouable, CoDepX, CoDepY);
+                        verifjouable(Menu.tabint, i, j, 0, -1, compteur, 1, ListeCoJouable, CoDepX, CoDepY);
 
                         compteur = 0;
-                        verifjouable(Menu.tabint, i,j, 0, 1, compteur, 1, ListeCoJouable, CoDepX, CoDepY);
+                        verifjouable(Menu.tabint, i, j, 0, 1, compteur, 1, ListeCoJouable, CoDepX, CoDepY);
                     }
                 }
             }
@@ -116,7 +117,7 @@ public class Methode_Main {
                 compteur++;
                 verifjouable(tab, newPosX, newposY, modx, mody, compteur, pion, listCo, CoDepX, CodepY);
             } else if (tab[newposY][newPosX] <= 0 && compteur >= 1) {
-                tab[newposY][newPosX]=tab[newposY][newPosX]-compteur;
+                tab[newposY][newPosX] = tab[newposY][newPosX] - compteur;
                 ArrayList<Integer> InfoCo = new ArrayList<>();
                 InfoCo.add(newPosX);
                 InfoCo.add(newposY);
@@ -124,21 +125,18 @@ public class Methode_Main {
                 InfoCo.add(CodepY);
                 InfoCo.add(compteur);
                 listCo.add(InfoCo);
-
             }
         } catch (Exception e) {
         }
     }
-    public static void ResetPosJouable (int[][]tab){
-        for (int i = 0; i < tab.length ; i++) {
-            for (int j = 0; j <tab[1].length ; j++) {
-                if (tab[i][j]<0){
-                    tab[i][j]=0;
+
+    public static void ResetPosJouable(int[][] tab) {
+        for (int i = 0; i < tab.length; i++) {
+            for (int j = 0; j < tab[1].length; j++) {
+                if (tab[i][j] < 0) {
+                    tab[i][j] = 0;
                 }
             }
-
         }
     }
 }
-
-
