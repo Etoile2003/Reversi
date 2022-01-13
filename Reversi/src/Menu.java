bd78dae646efdab545010e05102295ec36a93142
Etoile2003 <bastien.pelletier01@gmail.com>
2022-01-13 14:29:03 +0100

    Menu

---
+++ Reversi/src/Menu.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Menu {
+
+    public static void menu() throws InterruptedException {
+
+
+        int ia ;
+        Scanner sc = new Scanner(System.in) ;
+        System.out.println(" voulez vous jouer contre un joueur ou contre l'ia ? \n 1. joueur \n 2. ia ");
+        do {
+            try{
+                ia = Integer.parseInt(sc.nextLine()) ;
+            }catch (Exception e){
+                ia = -1 ;
+            }
+
+        }while(ia <1 || ia >2 ) ;
+        if (ia == 1){
+            Game.myGame(false);
+        }
+        else
+            Game.myGame(true);
+    }
+
+}
