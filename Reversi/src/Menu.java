7c40daae5ef6b17145b313d87ac980478516da50
Etoile2003 <bastien.pelletier01@gmail.com>
2022-01-14 07:17:11 +0100

    Quitter

--- Reversi/src/Menu.java
+++ Reversi/src/Menu.java
@@ -7,7 +7,7 @@ public class Menu {
 
         int ia ;
         Scanner sc = new Scanner(System.in) ;
-        System.out.println(" Voulez vous jouer contre un joueur ou contre l'ia ? \n 1. joueur \n 2. ia ");
+        System.out.println(" Voulez vous jouer contre un joueur ou contre l'ia ? \n 1. joueur \n 2. ia \n 3. Quitter ");
         do {
             try{
                 ia = Integer.parseInt(sc.nextLine()) ;
